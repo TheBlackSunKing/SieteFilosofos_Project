f7fda4d52a340b8b51c1465480102003bd5ca74c
José Emilio Yépez <j.e.ydarkserver@gmail.com>
2022-04-08 20:31:01 -0400

    Modificado HelloApliccations.jave y agregar una nota para despues cambiarle el nombre

--- src/main/java/uneg_team_so/sietefilosofos/HelloApplication.java
+++ src/main/java/uneg_team_so/sietefilosofos/HelloApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application { //TODO Cambiar nombre de esta clase
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 420);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 300);
+        stage.setTitle("Tabla");
         stage.setScene(scene);
         stage.show();
     }
